8e3462554903b3e77376b1bf85b1eb2215fa3286
liangjiang <xuan03121994@163.com>
2019-12-10 17:35:55 +0800

    react

--- src/index.js
+++ src/index.js
@@ -2,32 +2,36 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN';
 
 import App from './App'
 import { mainRouter } from './routes'
 
 render(
-    <Router>
-        <Switch>
-            <Route path='/admin' render={
-                (routerProps)=>{
-                    return <App {...routerProps} />
+    <ConfigProvider locale={zhCN}>
+        <Router>
+            <Switch>
+                <Route path='/admin' render={
+                    (routerProps)=>{
+                        return <App {...routerProps} />
+                    }
+                } />
+                {
+                    mainRouter.map(route=>{
+                        return (
+                            <Route 
+                                key={route.pathname}
+                                path={route.pathname}
+                                component={route.component}    
+                            />
+                        )
+                    })
                 }
-            } />
-            {
-                mainRouter.map(route=>{
-                    return (
-                        <Route 
-                            key={route.pathname}
-                            path={route.pathname}
-                            component={route.component}    
-                        />
-                    )
-                })
-            }
-            <Redirect to='/admin' from='/' exact />
-            <Redirect to='/404' />
-        </Switch>
-    </Router>,
+                <Redirect to='/admin' from='/' exact />
+                <Redirect to='/404' />
+            </Switch>
+        </Router>
+    </ConfigProvider>,
     document.querySelector('#root')
 );
--- src/views/News/index.js
+++ src/views/News/index.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react'
+import { Card, Button, Table } from 'antd'
+
+const dataSource = [
+    {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+  ];
+  
+  const columns = [
+    {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+    },
+    {
+        title: '操作',
+        dataIndex: 'actions',
+        key: 'actions',
+        render: (text, record, index)=>{
+            console.log({text, record, index})
+            return <Button>编辑</Button>
+        }
+    },
+  ];
 
 export default class NewsList extends Component {
     render() {
         return (
-            <div>
-                新闻列表...
-            </div>
+            <Card title="公告列表" bordered={false} extra={<Button>操作</Button>}>
+                <Table 
+                    dataSource={dataSource} 
+                    columns={columns} 
+                    loading={false}
+                    pagination={{
+                        hideOnSinglePage: true
+                    }}
+                />
+            </Card>
         )
     }
 }
